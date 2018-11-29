6a744e79169be0c028cf66a029963e86052730fb
Tom Patterson <tom@nextcapital.com>
2018-11-29 15:44:09 -0500

    Implementation of an MLB client

---
+++ server/app/lib/mlb_client/scoreboard.rb
@@ -0,0 +1,31 @@
+module MlbClient
+  class Scoreboard < Base
+    GAME_BASE_URL = "#{BASE_URL}/components/game/mlb"
+
+    attr_reader :year, :month, :day
+
+    def initialize(year, month, day)
+      @year = year
+      @month = month
+      @day = day
+    end
+
+    def fetch
+      scoreboard = get(scoreboard_url)
+      scoreboard.dig("data", "games", "game")
+    end
+
+    private
+
+    # year_2015/month_10/day_12/master_scoreboard.json
+    def scoreboard_url
+      [
+        GAME_BASE_URL,
+        "year_#{year}",
+        "month_#{month}",
+        "day_#{day}",
+        "master_scoreboard.json",
+      ].join("/")
+    end
+  end
+end

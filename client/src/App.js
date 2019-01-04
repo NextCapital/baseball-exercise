74a74eadc13380ab8767c45e877f5b8454366b6a
Henry Firth <firthh@firthh-00185.blc-hq.com>
2019-01-04 12:11:27 -0600

    Install axios and hook up a super basic client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import client from './client';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { games: [] };
+  }
+
+  // TODO: This should probably be moved to client.js
+  getData(relativePath) {
+    client.get(relativePath).then(({ data, status }) => {
+      this.setState({ games: data });
+    });
+  }
+
   render() {
+    // TODO: This should also probably be in client.js
+    const getGames = () => {
+      this.getData('games/index');
+    };
+
+    // TODO: This should likely be a stateless, 'Game' component
+    const renderGame = ({ away_team, home_team }) => {
+      return (
+        <div>{`${away_team.name} @ ${home_team.name}`}</div>
+      );
+    };
+
+    // TODO: This should also probably be its own, stateless component
+    const renderGames = (games = []) => {
+      if (games.length === 0) {
+        return (<div>No games to display</div>);
+      }
+
+      return games.map(renderGame);
+    };
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        { renderGames(this.state.games) }
+        <button onClick={ getGames }>Get games</button>
       </div>
     );
   }
